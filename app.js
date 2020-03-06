a190056029d8ffe77611cafeb7764a4a2b1952e8
chantoku <y.tokuyama.0622@gmail.com>
2020-03-06 18:36:15 +0900

    create project

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 3000;
+
+const config = {
+	channelSecret: process.env.CHANNEL_SECRET,
+	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+};
+
+const app = express();
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+	console.log(req.body.events);
+
+	if (req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff') {
+		res.send('Hello LINE BOT!(POST)');
+		console.log('疎通確認用');
+		return;
+	}
+
+	Promise
+		.all(req.body.events.map(handleEvent))
+		.then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+
+function handleEvent(event) {
+	if (event.type !== 'message' || event.message.type !== 'text') {
+		return Promise.resolve(null);
+	}
+
+	let mes = '';
+
+	return client.replyMessage(event.replyToken, {
+		type: 'text',
+		text: mes
+	});
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
