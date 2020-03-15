e5b83b0c199b03287e601e3b986097e52a8c5377
chantoku <y.tokuyama.0622@gmail.com>
2020-03-15 15:04:15 +0900

    add: ヘルプと一覧機能を実装

--- controllers/main.js
+++ controllers/main.js
@@ -2,35 +2,47 @@
 
 require("dotenv").config();
 
+const LIST = require(process.env.DIR + 'helpers/list.js');
 const CHARACTER = require(process.env.DIR + 'helpers/character.js');
 const MOVE = require(process.env.DIR + 'helpers/move.js');
 const STATUS = require(process.env.DIR + 'helpers/status.js');
 
 exports.getData = function(req) {
 	let mes = '';
-	let character = '';
-	let move = '';
-	let status = '';
+	const req_arr = req.split(/\s/);
 
-	// helpや一覧を取得
+	// ヘルプ
+	if (req_arr[0] == 'ヘルプ') {
+		mes += `「キャラ名 技名」で性能検索ができます。\n技名がわからない時は「キャラ名 一覧」で技一覧を参照できます。`
+	}
 
 	// キャラクター名を取得
-	character = CHARACTER.getCharacter(req[0]);
-	if (!character) {
-        mes += "キャラクター名が見つかりませんでした。";
+	let character = '';
+	if (!mes) {
+		character = CHARACTER.getCharacter(req_arr[0]);
+		if (!character) {
+        	mes += "キャラクター名が見つかりませんでした。";
+		}
+	}
+
+	// 一覧
+	if (character !== '' && req_arr[1] == '一覧') {
+		const list = LIST.getList(character);
+		mes += `${character}の技一覧\n\n${list}\n\n「${character} 技名」で性能を検索できます。`;
 	}
 
 	// 技名を取得
-	if (character !== '') {
-		move = MOVE.getMove(character, req[1]);
+	let move = '';
+	if (mes == '' && character !== '') {
+		move = MOVE.getMove(character, req_arr[1]);
 		if (character !== undefined && !move) {
-			mes += `技名が見つかりませんでした。キャラの技一覧は「${character} 技一覧」で調べることができます。\n\n`;
+			mes += `技名が見つかりませんでした。キャラの技一覧は「${character} 一覧」で調べることができます。\n\n`;
 		}
 	}
 
 	// 対応するステータスを取得
+	let status = '';
 	if (character !== '' && move !== '') {
-		console.log(move);
 		status = STATUS.getStatus(character, move);
 		if (!status) {
 			mes += "データが未実装です。運営にお問い合わせください。";
---
+++ helpers/list.js
@@ -0,0 +1,30 @@
+'use strict';
+
+require("dotenv").config();
+
+const commonData = require(process.env.DIR + "src/commonList.js");
+const commonList = commonData.commonList;
+
+const skillData = require(process.env.DIR + "src/skillList.js");
+const skillList = skillData.skillList;
+
+const moveData = require(process.env.DIR + "src/moveList.js");
+const moveList = moveData.moveList;
+
+exports.getList = function (character) {
+	let list = '';
+	for (let i = 0, len = moveList[character].length; i > len; i++) {
+		list += `${moveList[i]}\n`;
+	}
+
+	for (const key in skillList[character]) {
+		list += `${key}\n`;
+	}
+
+	if (list !== '') {
+		for (const key in commonList) {
+			list += `${key}\n`;
+		}
+	}
+	return list;
+}
